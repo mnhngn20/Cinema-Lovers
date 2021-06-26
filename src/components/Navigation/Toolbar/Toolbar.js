e0e352d38ce0feb6506350c1ff58a6620e0d7b05
Minh Nguyen <mnhngn20@gmail.com>
2021-06-26 20:41:29 +0700

    fix ui search bar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {connect} from 'react-redux';
 
 import SearchBar from '../../SearchBar/SearchBar';
+import SearchBarForMobile from '../../SearchBar/SearchBarForMobile';
 import NavigationItems from './NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
 import Modal from '../../UI/Modal/Modal';
@@ -9,6 +10,7 @@ import * as actions from '../../../store/actions/index';
  
 const Toolbar = props => {
     const [logoutModal, setLogoutModal] = useState(false);
+    const [showSearchBar, setShowSearchBar] = useState(false)
     const redirect = () => {
         props.onLogout()
         setLogoutModal(false)
@@ -22,15 +24,20 @@ const Toolbar = props => {
                 modalType="Success" >
                 <p>You Logged out.</p>
             </Modal>
-            <div className={classes.Toolbar}>
-                <div className={[classes.Logo, classes.MobileOnly].join(' ')} onClick={props.clicked}>
-                    <p className={classes.LogoMain}>Cinema</p>
-                </div>
+            <div className={[classes.Toolbar,showSearchBar ? classes.CenterSearchBar : null].join(' ')}>
+                {showSearchBar ? null : <div className={[classes.Logo, classes.MobileOnly].join(' ')} onClick={props.clicked}>
+                    <p className={classes.LogoMain}>Cinema.</p>
+                </div>}
                 <div className={[classes.Logo, classes.DesktopOnly].join(' ')}>
                     <p className={classes.LogoMain}>Cinema</p>
                     <p className={classes.LogoSub}>Lovers</p>
                 </div>
-                <SearchBar />
+                <div className={classes.SearchBarForDesktop}>
+                    <SearchBar />
+                </div>
+                <div className={classes.SearchBarForMobile}>
+                    <SearchBarForMobile setShowSearchBar={setShowSearchBar}/>
+                </div>
                 <div className={classes.DesktopOnly}>
                     <NavigationItems logout={() => setLogoutModal(true)}/>
                 </div>
---
+++ src/components/SearchBar/SearchBarForMobile.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import classes from './SearchBarForMobile.module.css';
+import SearchDropDownItems from './SearchDropDownItems/SearchDropDownItems';
+import searchIcon from '../../assets/search-icon.png';
+
+const SearchBarForMobile = ({setShowSearchBar}) => {
+    const [searchInput, setSearchInput] = useState('');
+    const [query, setQuery] = useState('');
+    const [show, setShow] = useState(false);
+    let inputRef = useRef();
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            if(searchInput === inputRef.current.value){
+                setQuery(inputRef.current.value);
+            }
+        }, 500);   
+        return ()=>{
+            clearTimeout(timer);
+        }
+    }, [inputRef, searchInput]);
+    
+    const inputChangeHandler = event => {
+        const newInput = event.target.value;
+        setSearchInput(newInput);
+    };
+    const showSearchBar = () => {
+        const isShow = show
+        setShow(!isShow);
+        setShowSearchBar(!isShow)
+    }
+    return (
+        <div className={classes.SearchBar}>
+            <div className={classes.Bar}>
+                <input
+                    ref = {inputRef}
+                    onChange = {(event)=> inputChangeHandler(event)}
+                    value = {searchInput}
+                    className={[classes.CustomSearchBar, show ? classes.SearchBarShow : classes.SearchBarHide].join(' ')} 
+                    type = 'text' 
+                    placeholder="" />
+                <img className={classes.SearchIcon} onClick={showSearchBar} src={searchIcon} alt="img"/>
+            </div>
+            {query !== '' && show ? <div className={classes.Items}>
+                <SearchDropDownItems  query={query}/>
+            </div> : null}
+        </div>
+    )
+}
+
+export default SearchBarForMobile;
